c7bbb8b4548812f4859dcf3a43222ef6f7a621f5
davidlehner1 <d.lehner2@students.htl-leonding.ac.at>
2023-05-30 15:52:27 +0200

    chore: made cld

--- app.ts
+++ app.ts
@@ -5,6 +5,7 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.use(express.static('public'));
 
 app.listen(3000, () => {
     console.log("Server listening on port 3000");
