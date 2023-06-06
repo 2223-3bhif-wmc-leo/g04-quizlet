5cc0c23202d6c4b2d66244112bc50921f73ea7be
davidlehner1 <d.lehner2@students.htl-leonding.ac.at>
2023-06-06 15:16:52 +0200

    chore: made database connection + data class

--- app.ts
+++ app.ts
@@ -1,5 +1,6 @@
 import express from "express";
 import cors from 'cors';
+import {DB, Unit} from "./data";
 
 const app = express();
 
@@ -7,6 +8,8 @@ app.use(cors());
 app.use(express.json());
 app.use(express.static('public'));
 
-app.listen(3000, () => {
+app.listen(3000, async () => {
     console.log("Server listening on port 3000");
+    const db = await DB.createDBConnection();
+    await db.close();
 });
---
+++ router/set-router.ts
@@ -0,0 +1,3 @@
+import express from "express";
+
+export const setRouter = express.Router();
