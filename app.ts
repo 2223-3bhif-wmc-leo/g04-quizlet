4963410cbe95a5862346fc009952d050c2c81a79
FroellerFelix <felix.froeller@gmail.com>
2023-06-08 14:12:19 +0200

    feat: Setup all routers to be ready to be worked on

--- app.ts
+++ app.ts
@@ -1,12 +1,19 @@
 import express from "express";
 import cors from 'cors';
-import {DB, Unit} from "./data";
+import {DB} from "./data";
+import {userRouter} from "./router/user-router";
+import {setElementRouter} from "./router/setElement-router";
+import {setRouter} from "./router/set-router";
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 app.use(express.static('public'));
+
+app.use("api/setElement", setElementRouter);
+app.use("api/set", setRouter);
+app.use("api/user", userRouter);
 
 app.listen(3000, async () => {
     console.log("Server listening on port 3000");
