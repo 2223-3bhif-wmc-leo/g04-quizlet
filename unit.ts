4d7ea5374cb7ab11093a20c8fbb36952ea43487d
FroellerFelix <felix.froeller@gmail.com>
2023-06-09 18:10:42 +0200

    feat: automated insertion of sample data

--- unit.ts
+++ unit.ts
@@ -54,7 +54,8 @@ export class Unit {
 
 export async function ensureSampleDataInserted(unit: Unit): Promise<void> {
     async function alreadyPresent(): Promise<boolean> {
-        const checkStmt = await unit.prepare(`SELECT COUNT(email) FROM user`);
+        const checkStmt = await unit.prepare(`SELECT COUNT(email)
+                                              FROM user`);
         const result = await checkStmt.get();
         const count = result['COUNT'];
 
@@ -63,10 +64,50 @@ export async function ensureSampleDataInserted(unit: Unit): Promise<void> {
 
     async function insert(): Promise<void> {
         // Prepare insert statements
-        // TODO: Prepare insert statements
+        const userInsertStmt = await unit.prepare(
+            `INSERT INTO main.user (email, password)
+             VALUES (?1, ?2)`
+        );
+        const setInsertStmt = await unit.prepare(
+            `INSERT INTO main."set" (setid, useremail, title, description, ispublic)
+             VALUES (?1, ?2, ?3, ?4, ?5)`
+        );
+        const setElementInsertStmt = await unit.prepare(
+            `INSERT INTO main.setelement (elementid, setid, word, definition)
+             VALUES (?1, ?2, ?3, ?4)`
+        );
 
-        // Insert sample data
-        // TODO: Insert sample data
+        // Insert sample data for User table
+        const user = [1, 'userpassword'];
+        await userInsertStmt.bind({
+            1: user[0],
+            2: user[1]
+        });
+        await userInsertStmt.run();
+        await userInsertStmt.reset();
+
+        // Insert sample data for Set table
+        const set = [1, 1, 'Sample Set', 'This is a sample set.', 1];
+        await setInsertStmt.bind({
+            1: set[0],
+            2: set[1],
+            3: set[2],
+            4: set[3],
+            5: set[4]
+        });
+        await setInsertStmt.run();
+        await setInsertStmt.reset();
+
+        // Insert sample data for SetElement table
+        const setElement = [1, 1, 'Sample Word', 'This is a sample word definition.'];
+        await setElementInsertStmt.bind({
+            1: setElement[0],
+            2: setElement[1],
+            3: setElement[2],
+            4: setElement[3]
+        });
+        await setElementInsertStmt.run();
+        await setElementInsertStmt.reset();
     }
 
     if (!await alreadyPresent()) {
