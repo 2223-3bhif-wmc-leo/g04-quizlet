7907454f97df3d8aad4a205eff1d43d25f40ae22
FroellerFelix <felix.froeller@gmail.com>
2023-06-09 20:41:42 +0200

    feat: implemented SetElement class

--- model/setElement-model.ts
+++ model/setElement-model.ts
@@ -1,6 +1,36 @@
-interface SetElement {
-    word: String;
-    definition: String;
+class SetElement {
+    // Fields
+    private _id: number;
+    private _word: String;
+    private _definition: String;
+
+    // Constructor
+
+    constructor(id: number, word: String, definition: String) {
+        this._id = id;
+        this._word = word;
+        this._definition = definition;
+    }
+
+    // Getters
+
+    getWord(): String {
+        return this._word;
+    }
+
+    getDefinition(): String {
+        return this._definition;
+    }
+
+    // Setters
+
+    setWord(word: String) {
+        this._word = word;
+    }
+
+    setDefinition(definition: String) {
+        this._definition = definition;
+    }
 }
 
 export {SetElement}
