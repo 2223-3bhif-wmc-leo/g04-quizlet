72460d44dc5025bc890e7dca33e77d5b605c4a17
FroellerFelix <felix.froeller@gmail.com>
2023-06-10 12:34:42 +0200

    feat: added repository-base.ts this class provides basic functions and exists to avoid code duplication

---
+++ repository/repository-base.ts
@@ -0,0 +1,30 @@
+import {Unit} from "../unit";
+import {Statement} from "sqlite";
+
+export class ServiceBase {
+    protected constructor(protected readonly unit: Unit) {
+
+    }
+
+    public async executeStmt(stmt: Statement): Promise<boolean> {
+        const result = await stmt.run();
+        return result.changes === 1;
+    }
+
+    protected static nullIfUndefined<T>(value: T | undefined): T | null {
+        if (value === undefined) {
+            return null;
+        }
+
+        return value;
+    }
+
+    protected static unwrapSingle<T>(obj: any | null | undefined, fieldName: string): T | null {
+        obj = ServiceBase.nullIfUndefined(obj);
+        return obj === null ? null : <T>obj[fieldName];
+    }
+
+    protected static unwrapAll<T>(obj: any[], fieldName: string): T[] {
+        return obj.map(o => <T>o[fieldName]);
+    }
+}
