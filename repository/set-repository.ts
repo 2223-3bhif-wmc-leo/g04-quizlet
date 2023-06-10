e1a6c4840054180a5272352503391f6b209c726a
davidlehner1 <d.lehner2@students.htl-leonding.ac.at>
2023-06-10 14:12:06 +0200

    feat: began set-repo. check if bug also stays on other environment

--- repository/set-repository.ts
+++ repository/set-repository.ts
@@ -11,11 +11,27 @@ class SetRepository extends RepositoryBase {
 
     // Methods
     public async updateSet(set: Set): Promise<void> {
-
+        const unit = await Unit.create(false);
+        try{
+            const stmt = await unit.prepare('UPDATE "Set" SET title = ?, description = ?, isPublic = ? WHERE setid = ?');
+            stmt.bind(set.getTitle(), set.getDescription(), set.getIsPublic(), set.getId());
+            await this.executeStmt(stmt);
+            await unit.complete(true);
+        } catch (e) {
+            await unit.complete(false);
+        }
     }
 
     public async insertSet(set: Set): Promise<void> {
-
+        const unit = await Unit.create(false);
+        try{
+            const stmt = await unit.prepare('INSERT INTO "Set" (userEmail, title, description, ispublic) VALUES (?, ?, ?, ?)');
+            stmt.bind(set.getTitle(), set.getDescription(), set.getIsPublic());
+            await this.executeStmt(stmt);
+            await unit.complete(true);
+        } catch (e) {
+            await unit.complete(false);
+        }
     }
 
     public async deleteSet(set: Set): Promise<void> {
