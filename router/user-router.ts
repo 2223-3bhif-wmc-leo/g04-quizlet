83af5591367a55b213be345fae44abcb03c40b9d
CaoAnton <antoncao1510@gmail.com>
2023-06-10 22:25:52 +0200

    feat: started with user-router and add test.http

--- router/user-router.ts
+++ router/user-router.ts
@@ -5,7 +5,7 @@ import {User} from "../model/user-model";
 import {StatusCodes} from "http-status-codes";
 
 export const userRouter = express.Router();
-userRouter.get(`/:email`, async (req, res) => {
+userRouter.get(`/user/:email`, async (req, res) => {
     const unit: Unit = await Unit.create(true);
     try {
         const userRepository = new UserRepository(unit);
@@ -21,4 +21,64 @@ userRouter.get(`/:email`, async (req, res) => {
     } finally {
         await unit.complete();
     }
+});
+userRouter.get(`/allUser`, async (req, res) => {
+    const unit: Unit = await Unit.create(true);
+    try {
+        const userRepository = new UserRepository(unit);
+        const result: User[] | null = await userRepository.getAllUsers();
+        if (result === null) {
+            res.status(StatusCodes.NOT_FOUND).send();
+        } else {
+            res.status(StatusCodes.OK).json(result);
+        }
+    } catch (e) {
+        console.log(e);
+        res.status(StatusCodes.INTERNAL_SERVER_ERROR).send();
+    } finally {
+        await unit.complete();
+    }
 })
+userRouter.put(`/user`, async (req, res) => {
+    const unit: Unit = await Unit.create(false);
+    try {
+        const userRepository = new UserRepository(unit);
+        const userExists: boolean = await userRepository.getUserByEmail(req.body.email) !== null;
+        const newUser: User = new User(req.body.email, req.body.password, []);
+        const success: boolean = userExists ? await userRepository.updateUser(newUser) : await userRepository.insertUser(newUser);
+        if (success) {
+            await unit.complete(true);
+            res.status(userExists ? StatusCodes.NO_CONTENT : StatusCodes.CREATED).json(newUser);
+        } else {
+            await unit.complete(false);
+            res.status(StatusCodes.BAD_REQUEST).send();
+        }
+    } catch (e) {
+        console.log(e);
+        res.status(StatusCodes.INTERNAL_SERVER_ERROR).send();
+    } finally {
+        await unit.complete(false);
+    }
+});
+userRouter.delete(`/user/:email`, async (req, res) => {
+    const unit: Unit = await Unit.create(true);
+    try {
+        const userRepository = new UserRepository(unit);
+        const getUser = await userRepository.getUserByEmail(req.params.email);
+        if (getUser === null) {
+            res.status(StatusCodes.NOT_FOUND).send();
+        } else {
+            const result: boolean = await userRepository.deleteUser(getUser);
+            if (result) {
+                res.status(StatusCodes.OK).send();
+            } else {
+                res.status(StatusCodes.NOT_FOUND).send();
+            }
+        }
+    } catch (e) {
+        console.log(e);
+        res.status(StatusCodes.INTERNAL_SERVER_ERROR).send();
+    } finally {
+        await unit.complete();
+    }
+});
