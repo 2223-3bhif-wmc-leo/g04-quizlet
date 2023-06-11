f2a139135b3585e212ad09e1cd1b082d523d6d5c
davidlehner1 <d.lehner2@students.htl-leonding.ac.at>
2023-06-11 15:13:13 +0200

    feat: register Screen finished

---
+++ public/register.ts
@@ -0,0 +1,36 @@
+interface userDb {
+    email: string,
+    password: string
+}
+document.addEventListener("DOMContentLoaded", async () => {
+    let passwordInputReg = <HTMLInputElement> document.getElementById("passwordInputReg");
+    let userNameInputReg = <HTMLInputElement> document.getElementById("userNameInputReg");
+    let inputForm = <HTMLElement> document.getElementById("inputForm");
+
+    inputForm.addEventListener("submit", async (event) => {
+        event.preventDefault();
+
+        let userInp: userDb = {
+            email: <string> userNameInputReg.value,
+            password: <string> passwordInputReg.value
+        }
+        console.log(userInp);
+        await fetchRestEndpoint('http://localhost:3000/api/user/userCreate', 'PUT', userInp);
+    });
+})
+
+async function fetchRestEndpoint(route: string, method: 'GET' |'POST' |'PUT' |'DELETE', data?: object): Promise<any> {
+    let options: any = { method };
+    if (data) {
+        options.headers = { 'Content-Type': 'application/json' };
+        options.body = JSON.stringify(data);
+    }
+    const res = await fetch(route, options);
+    if (!res.ok) {
+        const error = new Error(`${method} ${res.url} ${res.status} (${res.statusText})`);
+        throw error;
+    }
+    if (res.status !== 204) {
+        return await res.json();
+    }
+}
--- router/user-router.ts
+++ router/user-router.ts
@@ -39,19 +39,53 @@ userRouter.get(`/allUser`, async (req, res) => {
         await unit.complete();
     }
 })
-userRouter.put(`/user`, async (req, res) => {
+userRouter.put(`/userUpdate`, async (req, res) => {
     const unit: Unit = await Unit.create(false);
     try {
         const userRepository = new UserRepository(unit);
         const userExists: boolean = await userRepository.getUserByEmail(req.body.email) !== null;
-        const newUser: User = new User(req.body.email, req.body.password, []);
-        const success: boolean = userExists ? await userRepository.updateUser(newUser) : await userRepository.insertUser(newUser);
-        if (success) {
-            await unit.complete(true);
-            res.status(userExists ? StatusCodes.NO_CONTENT : StatusCodes.CREATED).json(newUser);
+        let success: boolean = false;
+        if(userExists){
+            const newUser: User = new User(req.body.email, req.body.password, []);
+            success = await userRepository.updateUser(newUser);
+            if (success) {
+                await unit.complete(true);
+                res.status(StatusCodes.NO_CONTENT).json(newUser);
+            } else {
+                await unit.complete(false);
+                res.status(StatusCodes.BAD_REQUEST).send();
+            }
         } else {
             await unit.complete(false);
-            res.status(StatusCodes.BAD_REQUEST).send();
+            res.status(StatusCodes.BAD_REQUEST).send({message: "User doesn't exist"});
+        }
+    } catch (e) {
+        console.log(e);
+        res.status(StatusCodes.INTERNAL_SERVER_ERROR).send();
+    } finally {
+        await unit.complete(false);
+    }
+});
+
+userRouter.put('/userCreate', async (req, res) => {
+    const unit: Unit = await Unit.create(false);
+    try {
+        const userRepository = new UserRepository(unit);
+        const userExists: boolean = await userRepository.getUserByEmail(req.body.email) !== null;
+        let success: boolean = false;
+        if(!userExists){
+            const newUser: User = new User(req.body.email, req.body.password, []);
+            success = await userRepository.insertUser(newUser);
+            if (success) {
+                await unit.complete(true);
+                res.status(StatusCodes.CREATED).json(newUser);
+            } else {
+                await unit.complete(false);
+                res.status(StatusCodes.BAD_REQUEST).send();
+            }
+        } else {
+            await unit.complete(false);
+            res.status(StatusCodes.BAD_REQUEST).send({message: "User already exists"});
         }
     } catch (e) {
         console.log(e);
